75083fcd0232b42f7bf703b50369872177e3ee46
David Marx <david.marx84@gmail.com>
2026-02-09 10:11:56 -0800

    Refactor test_annotations.py for clarity
    Comment out old flag check and assert for clarity.

--- tests/integration/test_annotations.py
+++ tests/integration/test_annotations.py
@@ -345,8 +345,9 @@ class TestPromptResponseAnnotatorIntegration:
         pr = clean_db_session.query(PromptResponse).first()
         
         #values = reader.get_string(EntityType.PROMPT_RESPONSE, pr.id, 'wiki_candidate')
-        values = reader.has_flag(EntityType.PROMPT_RESPONSE, pr.id, 'wiki_candidate') # todo: simplify/reconcile api
-        assert len(values) > 0
+        #values = reader.has_flag(EntityType.PROMPT_RESPONSE, pr.id, 'wiki_candidate') # todo: simplify/reconcile api
+        #assert len(values) > 0
+        assert reader.has_flag(EntityType.PROMPT_RESPONSE, pr.id, 'wiki_candidate')
     
     def test_naive_title_annotator_end_to_end(self, clean_db_session, wiki_conversation):
         """Test NaiveTitleAnnotator with real database."""
